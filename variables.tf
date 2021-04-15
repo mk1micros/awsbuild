f4d77638bf70a408d2e6514c13d323be2d596b45
Mark <markusroberts@hotmail.co.uk>
2021-04-15 19:10:43 +0100

    teraform setup

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "profile" {
+	type = string
+	default = "default"
+}
+variable "region-master" {
+	type = string
+	default = "eu-west-2"
+}
+
+variable "region-worker" {
+        type = string
+        default = "eu-west-1"
+}
