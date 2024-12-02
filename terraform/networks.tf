cc56ba3a5e15c537a3d5ea4dd4ecca46e2138d5c
mark roberts <mark.roberts@digital.justice.gov.uk>
2024-12-02 10:15:38 +0000

    remove ireland region

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -11,17 +11,7 @@ resource "aws_vpc" "vpc_master" {
 
 }
 
-#Create VPC in eu-west-1
-resource "aws_vpc" "vpc_ireland" {
-  provider             = aws.region-worker
-  cidr_block           = "10.200.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    "Name" = "Rust backup"
-  }
 
-}
 
 #Create IGW in eu-west-2
 resource "aws_internet_gateway" "igw-london" {
@@ -41,11 +31,7 @@ resource "aws_ec2_instance_connect_endpoint" "master_vpc_endpoint" {
   )
 }
 
-#Create IGW in eu-west-1
-resource "aws_internet_gateway" "igw-ireland" {
-  provider = aws.region-worker
-  vpc_id   = aws_vpc.vpc_ireland.id
-}
+
 
 #Get all available AZ's in VPC for master region
 data "aws_availability_zones" "azs" {
@@ -71,28 +57,8 @@ resource "aws_subnet" "subnet_2" {
 }
 
 
-#Create subnet in eu-west-2
-resource "aws_subnet" "subnet_1_london" {
-  provider   = aws.region-worker
-  vpc_id     = aws_vpc.vpc_ireland.id
-  cidr_block = "10.200.1.0/24"
-}
 
-#Initiate Peering connection request from eu-west-1
-resource "aws_vpc_peering_connection" "euwest1-euwest2" {
-  provider    = aws.region-master
-  peer_vpc_id = aws_vpc.vpc_ireland.id
-  vpc_id      = aws_vpc.vpc_master.id
-  peer_region = var.region-worker
 
-}
-
-#Accept VPC peering request in eu-west-2 from eu-west-1
-resource "aws_vpc_peering_connection_accepter" "accept_peering" {
-  provider                  = aws.region-worker
-  vpc_peering_connection_id = aws_vpc_peering_connection.euwest1-euwest2.id
-  auto_accept               = true
-}
 
 #Create route table in eu-west-2
 resource "aws_route_table" "internet_route" {
@@ -101,10 +67,6 @@ resource "aws_route_table" "internet_route" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw-london.id
-  }
-  route {
-    cidr_block                = "10.200.1.0/24"
-    vpc_peering_connection_id = aws_vpc_peering_connection.euwest1-euwest2.id
   }
   lifecycle {
     ignore_changes = all
@@ -121,32 +83,6 @@ resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
   route_table_id = aws_route_table.internet_route.id
 }
 
-#Create route table in eu-west-1
-resource "aws_route_table" "internet_route_london" {
-  provider = aws.region-worker
-  vpc_id   = aws_vpc.vpc_ireland.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw-ireland.id
-  }
-  route {
-    cidr_block                = "10.0.1.0/24"
-    vpc_peering_connection_id = aws_vpc_peering_connection.euwest1-euwest2.id
-  }
-  lifecycle {
-    ignore_changes = all
-  }
-  tags = {
-    Name = "Master-Region-RT"
-  }
-}
-
-#Overwrite default route table of VPC(Worker) with our route table entries
-resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
-  provider       = aws.region-worker
-  vpc_id         = aws_vpc.vpc_ireland.id
-  route_table_id = aws_route_table.internet_route_london.id
-}
 
 resource "aws_kms_key" "flowlog_kms_key" {
   description             = "VPC Flow Logs Encryption Key"
